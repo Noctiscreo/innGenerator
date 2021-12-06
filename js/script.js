e8aefa0bc8e9f2d39fd5181653d4a933dbe7dc6a
Noctiscreo <noctiscreo@gmail.com>
2021-12-06 21:54:15 +0000

    Created seperate functions for generating parts of inn names.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,34 @@
-function generateInnName() {
-    let randomInnFirstName = innFirstName[Math.floor(Math.random() * innFirstName.length)];
-    let randomInnSecondName = innSecondName[Math.floor(Math.random() * innSecondName.length)];
-    let randomInnName = randomInnFirstName + " " + randomInnSecondName;
-    document.getElementById("innName").innerHTML = randomInnName;
+// Generates a first name for the inn:
+function generateInnFirstName() {
+    let randomInnFirstName = innFirstName[Math.floor(Math.random() * innFirstName.length)];    
+    return randomInnFirstName;
 }
+// Generates a second name for the inn:
+function generateInnSecondName () {
+    let randomInnSecondName = innSecondName[Math.floor(Math.random() * innSecondName.length)];
+    return randomInnSecondName;
+}
+
+// Pick a random number from 1-8.
+// The result is used in determining how to combine the inn names.
+
+// Chooses how to combine the two names, using the random number above.
+
+// **TO DO: 
+// 1. Create variable for 'get element by ID'
+// 2. Write a Switch statement
+function combineInnNames () {
+    let randNumber = Math.floor(Math.random() * 3);
+    if (randNumber == 0) {
+        document.getElementById("innName").innerHTML = "The " + generateInnFirstName() + " " + generateInnSecondName();
+    }
+    if (randNumber == 1){
+        document.getElementById("innName").innerHTML = "The inn of the " + generateInnFirstName() + " " + generateInnSecondName();
+    }
+    if (randNumber == 2){
+        document.getElementById("innName").innerHTML = generateInnFirstName() + " " + generateInnSecondName();
+    }
+}
+
+
 
